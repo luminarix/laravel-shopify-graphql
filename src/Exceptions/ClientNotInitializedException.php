587684eadc92a9db56a3ab5e29cce1cba731813e
Mark <14284867+xHeaven@users.noreply.github.com>
2024-05-11 02:37:26 +0200

    Add defaults

--- src/Exceptions/ClientNotInitializedException.php
+++ src/Exceptions/ClientNotInitializedException.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Luminarix\Shopify\GraphQLClient\Exceptions;
 
 use Exception;
+use Throwable;
 
 class ClientNotInitializedException extends Exception
 {
+    public function __construct(string $message = 'Connector is not set', int $code = 428, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
