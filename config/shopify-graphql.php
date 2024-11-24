8740b805b0aad4f777d8d5130ab722b50f13b359
Mark <14284867+xHeaven@users.noreply.github.com>
2024-11-24 21:46:02 +0100

    Update configuration options

--- config/shopify-graphql.php
+++ config/shopify-graphql.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'api_version' => env('SHOPIFY_API_VERSION', '2024-04'),
+    'api_version' => env('SHOPIFY_API_VERSION', '2024-07'),
+    'fail_on_throttled' => env('SHOPIFY_FAIL_ON_THROTTLED', true),
+    'throttle_max_tries' => env('SHOPIFY_THROTTLE_MAX_TRIES', 5),
 ];
